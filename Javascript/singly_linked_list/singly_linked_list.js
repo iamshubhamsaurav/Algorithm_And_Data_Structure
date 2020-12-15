ddad4892d599e1daee227e4522badd63922942b0
Shubham Saurav <shubhamsinha186@gmail.com>
2020-12-15 20:09:17 +0530

    SingleLinkedList: pop and push done

--- Javascript/singly_linked_list/singly_linked_list.js
+++ Javascript/singly_linked_list/singly_linked_list.js
@@ -1,3 +1,53 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 class SinglyLinkedList {
-    
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    traverse() {
+        let current = this.head;
+        while(current) {
+            console.log(current)
+            current = current.next
+        }
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = this.head;
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
