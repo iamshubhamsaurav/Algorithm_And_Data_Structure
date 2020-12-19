e8cd4622e8127ec0756874b5ee36d7f39e8b9959
Shubham Saurav <shubhamsinha186@gmail.com>
2020-12-19 19:43:20 +0530

    Added unshift() method

--- Javascript/doubly_linked_list/basic.js
+++ Javascript/doubly_linked_list/basic.js
@@ -57,4 +57,18 @@ class DoublyLinkedList {
         return nodeToRemove;
     }
 
+    unshift(val) {
+        const newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.previous = newNode;
+            this.head = newNode;
+        }
+        this.length++;
+        return newNode;
+    }
+
 }
