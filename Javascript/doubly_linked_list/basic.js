9806ce8d8ac449dd6e3ec130a5f0b64c18368862
Shubham Saurav <shubhamsinha186@gmail.com>
2020-12-19 11:53:09 +0530

    Created Node and DoublyLinkedList classes

---
+++ Javascript/doubly_linked_list/basic.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
