65d375ac2f9beea4e92e0b17ba07d6b461aa2c36
Shubham Saurav <shubhamsinha186@gmail.com>
2020-12-19 18:21:19 +0530

    Added push and posh method

--- Javascript/doubly_linked_list/basic.js
+++ Javascript/doubly_linked_list/basic.js
@@ -1,6 +1,6 @@
 class Node {
-    constructor(value) {
-        this.value = value;
+    constructor(val) {
+        this.val = value;
         this.next = null;
         this.previous = null;
     }
@@ -11,5 +11,34 @@ class DoublyLinkedList {
         this.head = null;
         this.tail = null;
         this.length = 0;
+    }
+    
+    push(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.previous = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (this.length === 0) return undefined;
+        let nodeToRemove = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else  {
+            this.tail = nodeToRemove.previous;
+            this.tail.next = null;
+            nodeToRemove.previous = null;
+        }
+        this.length--;
+        return nodeToRemove;
     }
 }
